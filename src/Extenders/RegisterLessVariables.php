e6b0e26efd6e5993be66424fab4c943dc6dbac53
Dem13n <idem13n@gmail.com>
2021-03-14 22:16:19 +0300

    fix

--- src/Extenders/RegisterLessVariables.php
+++ src/Extenders/RegisterLessVariables.php
@@ -21,8 +21,8 @@ class RegisterLessVariables implements ExtenderInterface
                     $encoded_settings = json_decode($settings->get('dem13n_discussion_cards'), true);
 
                     $vars = [
-                        'desktop-card-width' => $encoded_settings['desktopCardWidth'] . '%',
-                        'tablet-card-width' => $encoded_settings['tabletCardWidth'] . '%',
+                        'desktop-card-width' => $encoded_settings['desktopCardWidth'] ??= '49' . '%',
+                        'tablet-card-width' => $encoded_settings['tabletCardWidth'] ??= '49' . '%',
                     ];
 
                     return array_reduce(array_keys($vars), function ($string, $name) use ($vars) {
@@ -32,8 +32,5 @@ class RegisterLessVariables implements ExtenderInterface
                 });
             });
         });
-
     }
-
-
 }
